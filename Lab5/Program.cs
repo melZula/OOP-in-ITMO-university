158b21f8342a44be44307c08b5a0530f969fc78f
Petr Melentev <41367520+melZula@users.noreply.github.com>
2019-11-24 20:54:15 +0300

    test

---
+++ Lab5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Data
+    {
+        SQLiteConnection Conn;
+        public bool DoCommand(string sql, bool writeOnly)
+        {
+            using(Conn = new SQLiteConnection("Data Source=" + @"..\..\database.db3" + ";Version=3;"))
+            {
+                Conn.Open();
+                SQLiteCommand command = new SQLiteCommand(sql, Conn);
+                if (!writeOnly)
+                {
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) Console.WriteLine(reader["name"]);
+                }
+                Conn.Close();
+            }
+            return false;
+        }
+        public void test()
+        {
+            string sql = "SELECT * FROM Shops";
+            DoCommand(sql, false);
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data datab = new Data();
+            datab.test();
+            Console.ReadKey();
+        }
+    }
+}
